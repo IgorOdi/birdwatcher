6cef324ed788e720220df8ff4a40e71d1e6c0ade
igorodi <igorodihw@gmail.com>
2020-03-08 13:09:40 -0300

    Scriptable databases for procedural generating birds

---
+++ Assets/Birdwatcher/_Scripts/Procedural/Database/BirdPartDatabase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Birdwatcher.Enum;
+using UnityEngine;
+
+namespace Birdwatcher.Database {
+
+    public class BirdPartDatabase<T> : ScriptableObject {
+
+        public T databaseType;
+        [SerializeField]
+        protected List<GameObject> partList = new List<GameObject> ();
+
+        public GameObject GetRandomPart (int seed) {
+
+            System.Random r = new System.Random (seed);
+            return partList[r.Next (0, partList.Count)];
+        }
+
+        public GameObject GetRandomPart () {
+
+            System.Random r = new System.Random ();
+            return partList[r.Next (0, partList.Count)];
+        }
+    }
+
+    [CreateAssetMenu (fileName = "New Beak Database", menuName = "Database/Bird/Beak")]
+    public class BirdBeakDatabase : BirdPartDatabase<Beak> { }
+
+    [CreateAssetMenu (fileName = "New Body Database", menuName = "Database/Bird/Body")]
+    public class BirdBodyDatabase : BirdPartDatabase<Body> { }
+
+    [CreateAssetMenu (fileName = "New Wing Database", menuName = "Database/Bird/Wing")]
+    public class BirdWingDatabase : BirdPartDatabase<Wings> { }
+
+    [CreateAssetMenu (fileName = "New Leg Database", menuName = "Database/Bird/Leg")]
+    public class BirdLegDatabase : BirdPartDatabase<Legs> { }
+}
---
+++ Assets/Birdwatcher/_Scripts/Procedural/Enum/BirdEnum.cs
@@ -0,0 +1,45 @@
+﻿namespace Birdwatcher.Enum {
+
+    public enum BirdPart {
+
+        BEAK,
+        BODY,
+        WINGS,
+        LEGS,
+        COLOR_PATTERN
+    }
+
+    public enum Beak {
+
+        GENERALIST,
+        INSECT,
+        GRAIN,
+        SEED,
+        NECTAR,
+        FRUIT,
+        CHISEL,
+        NETTING,
+        AERIAL_FISHING,
+        RAPTORIAL
+    }
+
+    public enum Body {
+
+        SMALL,
+        MEDIUM,
+        BIG,
+        HUGE,
+    }
+
+    public enum Wings {
+
+        FAST, //Good for zooming. Ex: Falcons
+        FLAPPY, //Good for darting around yards. Ex: Sparrows
+        SWOOPY, //Good for taking off and circling. Ex: Big Hawks, Eagles and Vultures
+        GULL, //Good for cruising very efficiently. Ex: Gulls and Albatrosses
+    }
+
+    public enum Legs {
+        
+    }
+}
